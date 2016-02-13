de5ba65a97e8cd7581d5aa7c4dc2711d294ab850
John Tompkins <tompkins.jt@gatech.edu>
2016-02-13 11:16:24 -0500

    Initial commit. Added matrix class with a few operations with tests.

---
+++ src/javaml/MathObjects/Vector.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaml.MathObjects;
+
+/**
+ *
+ * @author John
+ */
+public class Vector {
+    private Matrix backingMatrix;
+    private int length;
+    private boolean isRow;
+    private boolean isColumn;
+    
+    public Vector(int length) {
+        
+    }
+}
