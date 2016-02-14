066fe10629e184bde962149bd03fed9af85a119e
John Tompkins <tompkins.jt@gatech.edu>
2016-02-14 18:55:03 -0500

    Tidying up

--- src/javaml/regression/BatchGradientDescent.java
+++ src/javaml/regression/BatchGradientDescent.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package regression;
+package javaml.regression;
 
 import dataObjects.DataElement;
 import dataObjects.DataSet;
