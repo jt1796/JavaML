81a121d17cb2fa9ab28b9f3f52a68a8185c480da
John Tompkins <tompkins.jt@gatech.edu>
2016-02-14 00:06:06 -0500

    Created test, but it fails.

---
+++ test/regression/BatchGradientDescentTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regression;
+
+import dataObjects.DataSet;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author John
+ */
+public class BatchGradientDescentTest {
+    
+    public BatchGradientDescentTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void TwoDimLinear() {
+        DataSet set = new DataSet(1);
+        set.addData(6, new double[]{3});
+        set.addData(8, new double[]{4});
+        set.addData(20, new double[]{10});
+        BatchGradientDescent batch = new BatchGradientDescent(set, 100, 1);
+        batch.execute();
+        assertEquals(2, batch.getCoeffs().get(1), 0.01);
+    }
+}
