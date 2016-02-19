2a6c03e9cfb5d4d66709cff61242c263bee6533a
John Tompkins <tompkins.jt@gatech.edu>
2016-02-18 20:46:17 -0500

    Inverse test passes.

--- test/javaml/tests/mathObjects/MatrixTest.java
+++ test/javaml/tests/mathObjects/MatrixTest.java
@@ -6,10 +6,6 @@
 package javaml.tests.mathObjects;
 
 import javaml.mathObjects.Matrix;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
