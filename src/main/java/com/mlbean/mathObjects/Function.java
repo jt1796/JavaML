468828d393b2c255ba628ac3ca7c484573eb0945
John Tompkins <tompkins.jt@gatech.edu>
2016-02-19 21:23:13 -0500

    BSD license

--- src/main/java/com/mlbean/mathObjects/Function.java
+++ src/main/java/com/mlbean/mathObjects/Function.java
@@ -23,33 +23,12 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package com.mlbean.regression;
-
-import com.mlbean.dataObjects.DataSet;
-import com.mlbean.mathObjects.Vector;
+package com.mlbean.mathObjects;
 
 /**
  *
  * @author John
  */
-public abstract class Regression {
-    protected double stepSize = 0;
-    protected int iterations = 0;
-    protected Vector coeff = null;
-    protected DataSet dataSet = null;
+public class Function {
     
-    public Regression(DataSet dataSet, int iterations, double stepSize) {
-        this.stepSize = stepSize;
-        this.iterations = iterations;
-        this.dataSet = dataSet;
-    }
-    
-    public abstract void execute();
-    
-    public Vector getCoeffs() {
-        if(null == coeff) {
-            throw new RuntimeException("Must call execute() before this call");
-        }
-        return coeff;
-    }
 }
