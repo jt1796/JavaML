b7ef19d1d31a55356ec36fb88509ec452d75a12a
John Tompkins <john.tompkins@slalom.com>
2018-03-29 14:34:28 -0400

    help command works

--- src/main/java/com/mlbean/shell/Command.java
+++ src/main/java/com/mlbean/shell/Command.java
@@ -7,40 +7,43 @@ package com.mlbean.shell;
 
 import java.io.IOException;
 import java.io.Console;
+import java.util.Scanner;
 
 /**
  *
  * @author john.tompkins
  */
 public class Command {
-    
+
     InputStrategy inputStrategy;
     String cmdTxt;
     String stdout;
-    
+
     public Command(InputStrategy inputStrategy) {
         this.inputStrategy = inputStrategy;
     }
-    
+
     public void setTxt(String txt) {
         this.cmdTxt = txt;
     }
-    
+
     public enum InputStrategy {
         STDIN,
         BUFFER
     }
-    
+
     public void fetch() throws IOException {
         if (inputStrategy == InputStrategy.STDIN) {
-            this.cmdTxt = System.console().readLine();
+            Scanner scan = new Scanner(System.in);
+            this.cmdTxt = scan.nextLine();
+            scan.close();
         }
     }
-    
+
     public void exec(Environment env) {
         env.appendHistory(this);
         Exec e = new Exec(env, cmdTxt);
         this.stdout = e.run();
     }
-    
+
 }
